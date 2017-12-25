520083d6f6fbbc74bf54eb07430e0a78ed840e41
JoeImJoe <1334942535@qq.com>
2017-12-25 23:37:52 +0800

    合并版本

--- app/src/utils/HttpClient.js
+++ app/src/utils/HttpClient.js
@@ -6,7 +6,7 @@ import request from 'superagent'
 
 // const LOCAL_SERVER = 'http://localhost:1155/';
 
- const LOCAL_SERVER = ' http://10.3.135.223:888/';
+ const LOCAL_SERVER = ' http://10.3.135.110:888/';
 
 
 
